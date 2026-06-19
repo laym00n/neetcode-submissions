b266e3f0753c62a4c590a96ccc7c4fbe6f72c402
Keshav Gill <keshavgill030@gmail.com>
2026-06-19 12:26:08 +0530

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] countBits(int n) {
+        
+        int[] output = new int[n+1];
+
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            int num = i;
+            while (num > 0) {
+                if ((num & 1) == 1) {
+                    count++;
+                }
+                num >>= 1;
+            }
+            output[i] = count;
+        }
+
+        return output;
+    }
+}
