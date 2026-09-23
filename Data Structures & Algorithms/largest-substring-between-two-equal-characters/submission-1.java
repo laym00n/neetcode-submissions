355e1a8fe3e9f120401350fa4a6d00f84cebf0f3
Keshav Gill <keshavgill030@gmail.com>
2026-09-23 14:53:22 +0530

    Add: largest-substring-between-two-equal-characters - submission-1

---
+++ Data Structures & Algorithms/largest-substring-between-two-equal-characters/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxLengthBetweenEqualCharacters(String s) {
+        
+        Map<Character, Integer> map = new HashMap<>();
+        int ans = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                ans = Math.max(ans, i - map.get(c) - 1);
+            }
+            else {
+                map.put(c, i);
+            }
+        }
+        return ans;
+    }
+}
